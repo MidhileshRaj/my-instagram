ea6db6e94106ff08c32ea2b9afd7874c1db13771
MidhileshRaj <midhileshraj01@gmail.com>
2024-05-14 16:50:10 +0530

    firebase connected ios,android and web

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:myinstagram/firebase_options.dart';
 import 'package:myinstagram/utils/colors.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
